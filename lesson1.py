f6cf19f470d250d8aa7107d379872a25c1ce9fe5
beyzend <imbizniz@gmail.com>
2020-03-14 22:26:27 -0700

    finished up lesson1

--- lesson1.py
+++ lesson1.py
@@ -9,14 +9,21 @@ P = px*N.i + py*N.j + pz*N.k
 Q = qx*N.i + qy*N.j + qz*N.k
 
 crossPQ = P.cross(Q)
-normSqr = crossPQ.dot(CrossPQ)
-expandNormSqr = expand(normSqr)
-collectNormSqr = collect(expandNormSqr, (qx**2, qy**2, qz**2))
+normSqr = crossPQ.dot(crossPQ)
 addExpr = px**2*qx**2 + py**2*qy**2 + pz**2*qz**2
-UnevalAddExpr = UnevaluatedExpr(-addExpr)
-normSqrExpr = expandNormSqr + addExpr + UnevalAddExpr
-collectExpr = collect(normSqrExpr, (qx**2, qy**2, qz**2))
+normSqrExpr = collect(collect(factor(normSqr + addExpr),
+                              (qx**2, qy**2, qz**2)),
+                      (px**2 + py**2 + pz**2)) + -addExpr
+expr0 = normSqrExpr.func()
+expr1 = normSqrExpr.func()
+# Break normSqrExpr as two subexpressions.
+for exprArg in normSqrExpr.args:
+    if (len([True for arg in exprArg.args if (arg
+                                              == (px**2 + py**2 + pz**2))])
+            != 0):
+        expr0 = normSqrExpr.func(expr0, exprArg)
+    else:
+        expr1 = normSqrExpr.func(expr1, exprArg)
 
-
-
-
+normSqrExpr = normSqrExpr.func(expr0, factor(expr1))
+display(normSqrExpr)
