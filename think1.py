5480a0dcffaa4cd6f1f025ff15855806a575dd87
beyzend <imbizniz@gmail.com>
2020-03-16 09:22:43 -0700

    updated think1.py

---
+++ think1.py
@@ -0,0 +1,15 @@
+import turtle
+window = turtle.Screen()
+window.bgcolor("lightgreen")
+window.title("Hello, Tess!")
+
+tess = turtle.Turtle()
+tess.color("blue")
+tess.pensize(3)
+
+tess.forward(50)
+tess.left(120)
+tess.forward(50)
+
+#window.done()
+turtle.done()
